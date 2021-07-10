bb2cf47b821a34963cebdd43bcadb3217b8b4d29
74mu1i <87230273+74mu1i@users.noreply.github.com>
2021-07-11 01:06:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "",
-    channel: "833853700803264555",
+    token: "ODYzNTQwOTc0MjI2NzY3OTEz.YOoZuw.FvM-oaSyGvRnZLJ0jy_1F0rP8w4",
+    channel: "841483736229871677",
     messageTemplate: "Slaw @everyone, **{author}** Vedio nwe bllawkrayawa✅ **{title}**!\n{url}",
     channel_id: "UCFbWvTkRe8Fq2aXmnIvLp9A",
     watchInterval: 30000
